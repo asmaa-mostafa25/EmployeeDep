abb6739c180d57b645db450c90a4e59502801b30
hvr33 <asmaalnager1872004@gmail.com>
2026-09-16 17:11:53 +0300

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,128 @@
+﻿using EmployeeDep.Models;
+using EmployeeDep.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDep.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        AppDbContext _appDbContext = new AppDbContext();
+        public async Task<IActionResult> Index()
+        {
+            var vm = await _appDbContext.Departments
+                .Select(r => new DepartmentWithEmpCount
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    ManagerName = r.ManagerName,
+                    EmpCount = r.Employees.Count,
+                })
+                .ToListAsync();
+
+            return View(vm);
+        }
+
+        public async Task<IActionResult> GetDepById(int id)
+        {
+            var dep = await _appDbContext.Departments
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return View("GetById", dep);
+        }
+
+        [HttpGet]
+        public IActionResult AddDepartment()
+        {
+            return View("AddDepartment");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDepartment(
+            AddDepartmentViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("AddDepartment", viewModel);
+            }
+
+            var dep = new Department
+            {
+                Name = viewModel.Name,
+                ManagerName = viewModel.ManagerName
+            };
+
+            _appDbContext.Departments.Add(dep);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditDepartment(int id)
+        {
+            var dep = await _appDbContext.Departments
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (dep == null)
+            {
+                return NotFound();
+            }
+            var viewModel = new AddDepartmentViewModel
+            {
+                
+                Name = dep.Name,
+                ManagerName = dep.ManagerName
+            };
+            return View("EditDepartment", viewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditDepartment(int id, AddDepartmentViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EditDepartment", viewModel);
+            }
+
+            var dep = await _appDbContext.Departments
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (dep == null)
+                return NotFound();
+
+            dep.Name = viewModel.Name;
+            dep.ManagerName = viewModel.ManagerName;
+
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            var dep = await _appDbContext.Departments
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (dep == null)
+                return NotFound();
+            _appDbContext.Departments.Remove(dep);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> ShowEmployees(int id)
+        {
+            var employees = await _appDbContext.Employees
+                .Where(e => e.DepartmentId == id)
+                .Select(e => new EmployeeWithDepartmentViewModel
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Age = e.Age,
+                    Salary = e.Salary,
+                    JobTitle = e.JobTitle,
+                    DepartmentName = e.Department.Name
+                })
+                .ToListAsync();
+
+            return View("ShowEmployees", employees);
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,212 @@
+﻿using EmployeeDep.Models;
+using EmployeeDep.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDep.Controllers
+{
+
+    public class EmployeeController : Controller
+    {
+        AppDbContext _appDbContext = new AppDbContext();
+
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _appDbContext.Employees
+                .Select(e => new EmployeeWithDepartmentViewModel
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Age = e.Age,
+                    Salary = e.Salary,
+                    JobTitle = e.JobTitle,
+                    DepartmentName = e.Department.Name
+                })
+                .ToListAsync();
+
+            return View(employees);
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddEmployee()
+        {
+            var departments = await _appDbContext.Departments.ToListAsync();
+
+            var vm = new EmployeeViewModel
+            {
+                Departments = departments.Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name
+                }).ToList()
+            };
+
+            return View("AddEmployee", vm);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var departments = await _appDbContext.Departments.ToListAsync();
+
+                viewModel.Departments = departments.Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name
+                }).ToList();
+
+                return View("AddEmployee", viewModel);
+            }
+
+
+            var employeeExists = await _appDbContext.Employees
+                .AnyAsync(e =>
+                    e.Name.ToLower() == viewModel.Name.ToLower() &&
+                    e.DepartmentId == viewModel.DepartmentId);
+
+            if (employeeExists)
+            {
+                ModelState.AddModelError(
+                    "Name",
+                    "An employee with the same name already exists in this department.");
+
+                var departments = await _appDbContext.Departments.ToListAsync();
+
+                viewModel.Departments = departments.Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name
+                }).ToList();
+
+                return View("AddEmployee", viewModel);
+            }
+
+
+            var employee = new Employee
+            {
+                Name = viewModel.Name,
+                Age = viewModel.Age,
+                Salary = viewModel.Salary,
+                JobTitle = viewModel.JobTitle,
+                DepartmentId = viewModel.DepartmentId
+            };
+
+
+            _appDbContext.Employees.Add(employee);
+
+            await _appDbContext.SaveChangesAsync();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditEmployee(int id)
+        {
+            var employee = await _appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if (employee == null)
+                return NotFound();
+
+            var departments = await _appDbContext.Departments
+                .ToListAsync();
+
+            var vm = new EmployeeViewModel
+            {
+                Name = employee.Name,
+                Age = employee.Age,
+                Salary = employee.Salary,
+                JobTitle = employee.JobTitle,
+                DepartmentId = employee.DepartmentId,
+
+                Departments = departments.Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name,
+                    Selected = d.Id == employee.DepartmentId
+                }).ToList()
+            };
+
+            return View("EditEmployee", vm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditEmployee(
+    int id,
+    EmployeeViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var departments = await _appDbContext.Departments
+                    .ToListAsync();
+
+                viewModel.Departments = departments.Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name,
+                    Selected = d.Id == viewModel.DepartmentId
+                }).ToList();
+
+                return View("EditEmployee", viewModel);
+            }
+
+            var employee = await _appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if (employee == null)
+                return NotFound();
+
+            var employeeExists = await _appDbContext.Employees
+                .AnyAsync(e =>
+                    e.Id != id &&
+                    e.Name.ToLower() == viewModel.Name.ToLower() &&
+                    e.DepartmentId == viewModel.DepartmentId);
+
+            if (employeeExists)
+            {
+                ModelState.AddModelError(
+                    "Name",
+                    "An employee with the same name already exists in this department.");
+
+                var departments = await _appDbContext.Departments
+                    .ToListAsync();
+
+                viewModel.Departments = departments.Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name,
+                    Selected = d.Id == viewModel.DepartmentId
+                }).ToList();
+
+                return View("EditEmployee", viewModel);
+            }
+
+            employee.Name = viewModel.Name;
+            employee.Age = viewModel.Age;
+            employee.Salary = viewModel.Salary;
+            employee.JobTitle = viewModel.JobTitle;
+            employee.DepartmentId = viewModel.DepartmentId;
+
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if (employee == null)
+                return NotFound();
+
+            _appDbContext.Employees.Remove(employee);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
